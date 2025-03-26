5670d4dcac09b84c3c4bc1c1e2d4924f1288a7da
D2PT <d2pt@TECHIN.INTERNAL>
2025-03-26 11:10:43 +0400

    Just a quick fix

--- RecipeBookTest/src/main/java/LoginPage.java
+++ RecipeBookTest/src/main/java/LoginPage.java
@@ -7,19 +7,33 @@ public class LoginPage {
     private By emailField = By.id("email");
     private By passwordField = By.id("password");
     private By loginButton = By.cssSelector("button[type='submit']");
-    private By errorMessage = By.id("errorMessage");
-
+    private By errorMessage = By.cssSelector(".form-input-error");
+    private By clickLoginPage = By.linkText("LOGIN");
+    private By otherErrorMessage = By.cssSelector(".error");
+    private By RequiredPasswordMessage = By.cssSelector("body > div:nth-child(1) > main:nth-child(2) > div:nth-child(1) > form:nth-child(1) > div:nth-child(4) > p:nth-child(3)");
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
     public void loginUser(String email, String password) {
-
+       driver.findElement(clickLoginPage).click();
         driver.findElement(emailField).sendKeys(email);
-
         driver.findElement(passwordField).sendKeys(password);
         driver.findElement(loginButton).click();
     }
 
+
+    public boolean isErrorMessageDisplayed() {
+        return driver.findElement(errorMessage).isDisplayed();
+    }
+    public String getErrorMessage(){
+        return driver.findElement(errorMessage).getText();
+    }
+    public boolean isOtherErrorMessageDisplayed() {
+        return driver.findElement(otherErrorMessage).isDisplayed();
+    }
+    public String getOtherErrorMessage(){
+        return driver.findElement(otherErrorMessage).getText();
+    }
 
 }
--- RecipeBookTest/src/test/java/LoginTest.java
+++ RecipeBookTest/src/test/java/LoginTest.java
@@ -1,7 +1,10 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LoginTest extends BaseTest {
 
@@ -9,15 +12,39 @@ public class LoginTest extends BaseTest {
     public void testUserLoginValid() {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.loginUser("john.doom@test.com", "Doom1234");
-
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.urlToBe("http://localhost:5173/"));
+        String CurrentURL = driver.getCurrentUrl();
+        assertEquals("http://localhost:5173/", CurrentURL, "Login did not redirect to home page" );
 
     }
 
     @Test
-    public void testUserLoginInvalid() {
+    public void testUserLoginInvalidEmail() {
+        LoginPage loginPage = new LoginPage(driver);
+       loginPage.loginUser("invalidtest.com", "WrongPass123");
+
+       assertTrue(loginPage.isErrorMessageDisplayed(), "Error message should be displayed for invalid login.");
+       String ErrorMessage = loginPage.getErrorMessage();
+       assertTrue(ErrorMessage.contains("Invalid email address"), "incorrect error message");
+    }
+    @Test
+    public void testUserLoginInvalidPassword(){
         LoginPage loginPage = new LoginPage(driver);
         loginPage.loginUser("invalid@test.com", "WrongPass123");
 
+        assertTrue(loginPage.isOtherErrorMessageDisplayed(), "Error message should be displayed for invalid login.");
+       
+    }
+    @Test
+    public void testUserLoginEmptyInputs(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.loginUser("", "");
+
         assertTrue(loginPage.isErrorMessageDisplayed(), "Error message should be displayed for invalid login.");
+
+        String ErrorMessage = loginPage.getErrorMessage();
+
+        assertTrue(ErrorMessage.contains("Email is required"), "incorrect error message");
+
     }
 }
